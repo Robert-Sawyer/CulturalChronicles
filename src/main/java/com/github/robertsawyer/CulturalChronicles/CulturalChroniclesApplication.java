4d09230ecb61bced5f80ab005fe8b5188b18a728
Robert Olbrychowski <47324286+Robert-Sawyer@users.noreply.github.com>
2019-06-19 16:39:01 +0200

    Create project: (#1)
    * Adjusted gitignore
    * Added Maven project structure
    * Added dependencies
    * Added Spring Boot Application class

---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/CulturalChroniclesApplication.java
@@ -0,0 +1,12 @@
+package com.github.robertsawyer.CulturalChronicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturalChroniclesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulturalChroniclesApplication.class, args);
+    }
+}
