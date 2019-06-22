043ff3aec896fd9f6ab283211ac75cbc1adcba81
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-06-22 13:03:03 +0200

    Added classes User and Role. * Added fields and adnotation. * Added method prePersist.

---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Role.java
@@ -0,0 +1,7 @@
+package com.github.robertsawyer.CulturalChronicles.domain.model;
+
+public class Role {
+
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/User.java
@@ -0,0 +1,37 @@
+package com.github.robertsawyer.CulturalChronicles.domain.model;
+
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class User {
+
+    private Long id;
+    private String login;
+    private String password;
+    private String email;
+    private LocalDate created;
+
+    @Column(name = "enabled")
+    private Boolean enabled = true;
+
+    @ManyToMany
+    @JoinTable(name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> role = new HashSet<>();
+
+    //private UserDetails userDetails;
+
+    private List<Movie> movies;
+   // private List<Book> books;
+    //private List<Game> games;
+
+    @PrePersist
+    public void prePersist() {
+        created = LocalDate.now();
+    }
+}
