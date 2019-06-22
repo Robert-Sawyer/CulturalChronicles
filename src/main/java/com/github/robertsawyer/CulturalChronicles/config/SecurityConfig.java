15c13a365e9975d89ceac8a5c2a5dd504e550551
Robert Olbrychowski <robert.olbrychowski@hotmail.com>
2019-06-22 12:43:08 +0200

    Added configuration methods: * configured access to pages depending on user's role * configured authentication JDBC

--- src/main/java/com/github/robertsawyer/CulturalChronicles/config/SecurityConfig.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/config/SecurityConfig.java
@@ -1,7 +1,73 @@
 package com.github.robertsawyer.CulturalChronicles.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder())
+                .usersByUsernameQuery("SELECT login, password, enabled FROM users WHERE login = ?")
+                .authoritiesByUsernameQuery("SELECT login, role FROM users INNER JOIN user_role \n" +
+                        "ON user.id = user_role.user_id \n" +
+                        "INNER JOIN roles \n" +
+                        "ON user_role.role_id = roles.id \n" +
+                        "WHERE users.login = ?");
+
+//        auth.inMemoryAuthentication()
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/login").anonymous()
+                .antMatchers("/user", "/user/**").hasRole("USER")
+                .antMatchers("/admin", "/admin**").hasRole("ADMIN")
+                .antMatchers("/media").permitAll()
+        //tutaj będą kolejne punkty dostępu do podstron
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .usernameParameter("login")
+                .passwordParameter("password")
+                .defaultSuccessUrl("/")
+                .and()
+            .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .and()
+            .csrf()
+                .disable()
+            .rememberMe()
+                .key("mojKlucz")
+                .rememberMeParameter("rememberMe")
+                .tokenValiditySeconds(7*24*60);
+    }
 }
