80e88368910c5be9124856906ee4985745e3a9de
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-07-02 21:05:46 +0200

    Added fields in UserDetails.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/UserDetails.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/UserDetails.java
@@ -2,5 +2,8 @@ package com.github.robertsawyer.CulturalChronicles.domain.model;
 
 public class UserDetails {
 
-
+    private Long id;
+    private String firstName;
+    private String lastName;
+    
 }
