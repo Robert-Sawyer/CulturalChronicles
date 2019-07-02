1d1808e0dcca0960a8e20fb81f41409121d5a3c9
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-07-02 20:58:50 +0200

    Added fields in Movie entity.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Movie.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Movie.java
@@ -1,4 +1,19 @@
 package com.github.robertsawyer.CulturalChronicles.domain.model;
 
+import java.util.List;
+
 public class Movie {
+
+    private Long id;
+    private String title;
+    private List<Director> director;
+    private List<Writer> writer;
+    private List<Producer> producers;
+    private Integer yearOfProduction;
+    private Double rating;
+    private Boolean favourite;
+    private Long duration;
+    private List<String> country;
+    private List<String> genre;
+
 }
