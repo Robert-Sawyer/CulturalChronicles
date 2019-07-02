294c4c4272f15d57fb09a98a8734aa52c1207e97
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-07-02 20:59:59 +0200

    Added entities Director, Writer and Producer.

---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Director.java
@@ -0,0 +1,11 @@
+package com.github.robertsawyer.CulturalChronicles.domain.model;
+
+public class Director {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer yearOfBirth;
+    private Integer yearOfDeath;
+    private Boolean favourite;
+}
---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Writer.java
@@ -0,0 +1,11 @@
+package com.github.robertsawyer.CulturalChronicles.domain.model;
+
+public class Writer {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer yearOfBirth;
+    private Integer yearOfDeath;
+    private Boolean favourite;
+}
