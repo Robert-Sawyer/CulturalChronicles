0ebdf98a0d48f7d06d123b81e02e535f72701b9a
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-07-11 20:19:21 +0200

    Added adnotation Controller and method homepage to screen the view home.jsp

--- src/main/java/com/github/robertsawyer/CulturalChronicles/web/controllers/HomeController.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/web/controllers/HomeController.java
@@ -1,4 +1,13 @@
 package com.github.robertsawyer.CulturalChronicles.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String homepage(){
+        return "home";
+    }
 }
