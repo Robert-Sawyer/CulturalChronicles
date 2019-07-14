fefee6272e5777935378f03b3459c9c28f77d803
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-07-14 22:48:53 +0200

    Added controller for registration process and method prepareRegisterForm in it.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/web/controllers/RegisterController.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/web/controllers/RegisterController.java
@@ -1,4 +1,18 @@
 package com.github.robertsawyer.CulturalChronicles.web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
 public class RegisterController {
+
+    @GetMapping
+    public String prepareRegistrationForm(Model model) {
+        return "home/register";
+    }
+
+
 }
