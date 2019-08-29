3d4ee673013430e33dae05d250b2949969b170e1
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-08-29 21:46:43 +0200

    Added post mapping method in Registration Controller.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/web/controllers/RegisterController.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/web/controllers/RegisterController.java
@@ -2,8 +2,13 @@ package com.github.robertsawyer.CulturalChronicles.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/register")
@@ -14,5 +19,20 @@ public class RegisterController {
         return "home/register";
     }
 
-
+//    @PostMapping
+//    public String processRegistration(@Valid @ModelAttribute("registrationForm") RegistrationFormDTO form, BindingResult result){
+//        if (result.hasErrors()){
+//            return "registration-page";
+//        }
+//        if(!checkPasswordEquality(form)){
+//            result.rejectValue("password", null, "Hasło i powtórzone hasło niezgodne");
+//            return "registration-page";
+//        }
+//        if(!checkIsUsernameIsAvailable(form)){
+//            result.rejectValue("login", null, "Nazwa użytkownika już zajęta");
+//            return "registration-page";
+//        }
+//        //Konwersja rfDTO na user encja i wywołąnie userrepositiry.save(...);
+//        userService.registerUser(form);
+//        return "redirect:/index.html";
 }
