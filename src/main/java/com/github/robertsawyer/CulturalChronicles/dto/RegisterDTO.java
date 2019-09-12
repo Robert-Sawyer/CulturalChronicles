c04a415a56e00312d245be5e3abec0d20f5ffc4f
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-12 21:05:46 +0200

    Added fields and annotations in RegisterDTO.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/dto/RegisterDTO.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/dto/RegisterDTO.java
@@ -1,4 +1,16 @@
 package com.github.robertsawyer.CulturalChronicles.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterDTO {
+
+    private String login;
+    private String email;
+    private String password;
+    private String confirPassword;
 }
