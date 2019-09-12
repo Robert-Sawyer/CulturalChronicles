d1ec563dbe30bd8fe7fe1091e900c83ee6d609e5
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-12 21:03:06 +0200

    Created class UserService.

---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/services/UserService.java
@@ -0,0 +1,10 @@
+package com.github.robertsawyer.CulturalChronicles.services;
+
+import com.github.robertsawyer.CulturalChronicles.dto.RegisterDTO;
+
+public class UserService {
+
+    public void registerUser(RegisterDTO form) {
+
+    }
+}
