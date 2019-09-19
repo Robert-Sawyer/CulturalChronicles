c2353dc289577f4193e4a3c5ed0cb7d592d8d03a
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-19 20:28:50 +0200

    Added fields in UserDTO.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/dto/UserDTO.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/dto/UserDTO.java
@@ -1,4 +1,9 @@
 package com.github.robertsawyer.CulturalChronicles.dto;
 
+import com.github.robertsawyer.CulturalChronicles.domain.model.UserDetails;
+
 public class UserDTO {
+
+    private String login;
+    private UserDetails userDetails;
 }
