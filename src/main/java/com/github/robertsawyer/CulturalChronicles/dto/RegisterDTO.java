7d1e52ff31c8d11a6497fd80d0a7589d64cb2bc4
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-19 20:31:43 +0200

    Added validation annotations in RegisterDTO.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/dto/RegisterDTO.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/dto/RegisterDTO.java
@@ -4,13 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterDTO {
 
+    @NotNull
+    @Size(min = 3, max = 20)
     private String login;
+
+    @NotNull
+    @Email
     private String email;
+
+    @NotNull
+    @Size(min = 3, max = 20)
     private String password;
-    private String confirPassword;
+
+    @NotNull
+    @Size(min = 3, max = 20)
+    private String confirmPassword;
+
+
 }
