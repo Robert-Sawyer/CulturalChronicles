d13f48c969e64233e8f1f0ae30261d0f4a9908ac
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-23 21:59:20 +0200

    Added getters and setters to RegisterDTO.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/dto/RegisterDTO.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/dto/RegisterDTO.java
@@ -29,5 +29,35 @@ public class RegisterDTO {
     @Size(min = 3, max = 20)
     private String confirmPassword;
 
+    public String getLogin() {
+        return login;
+    }
 
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
