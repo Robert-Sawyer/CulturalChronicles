b90f13da88bccb8188832312e1f13309fed88b68
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-29 10:05:31 +0200

    Created AddMovieDTO with fields, getters and setters.

---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/dto/AddMovieDTO.java
@@ -0,0 +1,54 @@
+package com.github.robertsawyer.CulturalChronicles.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddMovieDTO {
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private List<String> genre;
+
+    private List<String> country;
+
+    private Integer productionYear;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getGenre() {
+        return genre;
+    }
+
+    public void setGenre(List<String> genre) {
+        this.genre = genre;
+    }
+
+    public List<String> getCountry() {
+        return country;
+    }
+
+    public void setCountry(List<String> country) {
+        this.country = country;
+    }
+
+    public Integer getProductionYear() {
+        return productionYear;
+    }
+
+    public void setProductionYear(Integer productionYear) {
+        this.productionYear = productionYear;
+    }
+}
