467a88e76c916eb9f0d2c701483e6b30beb90218
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-29 10:07:58 +0200

    Modified Movie class, added annotation.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Movie.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Movie.java
@@ -1,7 +1,14 @@
 package com.github.robertsawyer.CulturalChronicles.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
 import java.util.List;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
 
     private Long id;
@@ -16,4 +23,91 @@ public class Movie {
     private List<String> country;
     private List<String> genre;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Director> getDirector() {
+        return director;
+    }
+
+    public void setDirector(List<Director> director) {
+        this.director = director;
+    }
+
+    public List<Writer> getWriter() {
+        return writer;
+    }
+
+    public void setWriter(List<Writer> writer) {
+        this.writer = writer;
+    }
+
+    public List<Producer> getProducers() {
+        return producers;
+    }
+
+    public void setProducers(List<Producer> producers) {
+        this.producers = producers;
+    }
+
+    public Integer getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(Integer yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public Boolean getFavourite() {
+        return favourite;
+    }
+
+    public void setFavourite(Boolean favourite) {
+        this.favourite = favourite;
+    }
+
+    public Long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Long duration) {
+        this.duration = duration;
+    }
+
+    public List<String> getCountry() {
+        return country;
+    }
+
+    public void setCountry(List<String> country) {
+        this.country = country;
+    }
+
+    public List<String> getGenre() {
+        return genre;
+    }
+
+    public void setGenre(List<String> genre) {
+        this.genre = genre;
+    }
 }
