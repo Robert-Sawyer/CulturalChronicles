b0e090449deb33b0a2cdd154e3e6d577efe33c85
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-29 10:03:49 +0200

    Added annotation in UserService.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/services/UserService.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/services/UserService.java
@@ -1,10 +1,17 @@
 package com.github.robertsawyer.CulturalChronicles.services;
 
 import com.github.robertsawyer.CulturalChronicles.dto.RegisterDTO;
+import com.github.robertsawyer.CulturalChronicles.dto.UserDTO;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     public void registerUser(RegisterDTO form) {
 
+    }
+
+    public UserDTO findUser(String login) {
+        return null;
     }
 }
