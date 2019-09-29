8a51cf7f686acee48894b38c4aac5052642fc8c8
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-09-29 10:06:16 +0200

    Autowired MovieRepository, added method creating new Movie.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/services/MovieService.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/services/MovieService.java
@@ -1,4 +1,22 @@
 package com.github.robertsawyer.CulturalChronicles.services;
 
+import com.github.robertsawyer.CulturalChronicles.domain.model.Movie;
+import com.github.robertsawyer.CulturalChronicles.domain.repositories.MovieRepository;
+import com.github.robertsawyer.CulturalChronicles.dto.AddMovieDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Transactional
+    public void createNewMovie(AddMovieDTO addMovieDTO) {
+        Movie movie = Converters.convertToNewMovie(addMovieDTO);
+        movieRepository.save(movie);
+    }
+
 }
