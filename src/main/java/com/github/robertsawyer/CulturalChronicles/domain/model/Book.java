e99e8bec71084a1c463fdc748796e1040d428036
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-10-03 21:01:50 +0200

    Added annotations, fields to Book entity.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Book.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Book.java
@@ -1,7 +1,60 @@
 package com.github.robertsawyer.CulturalChronicles.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     private Long id;
     private String title;
+    private Author author;
+    private Long pages;
+    private Long yearOfPublish;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Long getPages() {
+        return pages;
+    }
+
+    public void setPages(Long pages) {
+        this.pages = pages;
+    }
+
+    public Long getYearOfPublish() {
+        return yearOfPublish;
+    }
+
+    public void setYearOfPublish(Long yearOfPublish) {
+        this.yearOfPublish = yearOfPublish;
+    }
 }
