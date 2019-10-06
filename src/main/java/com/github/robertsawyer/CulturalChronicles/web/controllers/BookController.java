d73ad0d691974ed1833a375fe1950d33af077bb2
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-10-06 21:28:43 +0200

    Added BookController with first methods and annotations.

---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/web/controllers/BookController.java
@@ -0,0 +1,45 @@
+package com.github.robertsawyer.CulturalChronicles.web.controllers;
+
+import com.github.robertsawyer.CulturalChronicles.dto.AddBookDTO;
+import com.github.robertsawyer.CulturalChronicles.services.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+    public static final Logger logger = LoggerFactory.getLogger(BookController.class);
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/add")
+    public String prepareAddNewBookForm(Model model) {
+        model.addAttribute("newBook", new AddBookDTO());
+        return "books/addBook";
+    }
+
+    @PostMapping("/add")
+    public String addNewBook(@Valid @ModelAttribute("book") AddBookDTO addBookDTO, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "books/addBook";
+        }
+        logger.info("Zapisuję nowa książkę do bazy danych.");
+
+        bookService.createNewBook(addBookDTO);
+        return "redirect:/dashboard";
+    }
+
+}
