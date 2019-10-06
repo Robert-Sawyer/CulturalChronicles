50e6ed44b332f43c389aecf0c4df2a6240b3c0af
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-10-06 14:01:52 +0200

    Added extension in BookRepository.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/domain/repositories/BookRepository.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/repositories/BookRepository.java
@@ -1,8 +1,9 @@
 package com.github.robertsawyer.CulturalChronicles.domain.repositories;
 
 import com.github.robertsawyer.CulturalChronicles.domain.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     Book findByTitle(String title);
 }
