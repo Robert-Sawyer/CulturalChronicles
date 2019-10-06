15f78a76836bfffeabcc4ee2bc966c0bb531b464
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-10-06 21:25:53 +0200

    Added converting method to create book.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/services/Converters.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/services/Converters.java
@@ -1,6 +1,8 @@
 package com.github.robertsawyer.CulturalChronicles.services;
 
+import com.github.robertsawyer.CulturalChronicles.domain.model.Book;
 import com.github.robertsawyer.CulturalChronicles.domain.model.Movie;
+import com.github.robertsawyer.CulturalChronicles.dto.AddBookDTO;
 import com.github.robertsawyer.CulturalChronicles.dto.AddMovieDTO;
 
 public class Converters {
@@ -13,5 +15,16 @@ public class Converters {
         movie.setGenre(addMovieDTO.getGenre());
         movie.setYearOfProduction(addMovieDTO.getProductionYear());
         return movie;
+    }
+
+    public static Book convertToNewBook(AddBookDTO addBookDTO) {
+        Book book = new Book();
+        book.setTitle(addBookDTO.getTitle());
+        book.setAuthor(addBookDTO.getAuthor());
+        book.setYearOfPublish(addBookDTO.getYearOfPublish());
+        book.setPublisher(addBookDTO.getPublisher());
+        book.setGenre(addBookDTO.getGenre());
+        book.setPages(addBookDTO.getPages());
+        return book;
     }
 }
