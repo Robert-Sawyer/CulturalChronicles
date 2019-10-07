b5db4cd809b5d022591c35dca7d2fb83f3abd5ed
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-10-07 22:09:58 +0200

    Created GameController with fields, annotations and methods.

---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/web/controllers/GameController.java
@@ -0,0 +1,49 @@
+package com.github.robertsawyer.CulturalChronicles.web.controllers;
+
+import com.github.robertsawyer.CulturalChronicles.dto.AddGameDTO;
+import com.github.robertsawyer.CulturalChronicles.services.GameService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/games")
+public class GameController {
+
+    public static final Logger logger = LoggerFactory.getLogger(GameController.class);
+
+    @Autowired
+    private GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @GetMapping("/add")
+    public String prepareAddNewGameForm(Model model) {
+        model.addAttribute("newGame", new AddGameDTO());
+        return "games/addGame";
+    }
+
+    @PostMapping("/add")
+    public String addNewGame(@Valid @ModelAttribute("game") AddGameDTO addGameDTO, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "games/addGame";
+        }
+
+        logger.info("Zapisuję nową grę do bazy");
+
+        gameService.createNewGame(addGameDTO);
+        return "redirect:/dashboard";
+    }
+}
