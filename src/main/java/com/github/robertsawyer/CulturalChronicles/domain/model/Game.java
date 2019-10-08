4f41f0999baff1464f0711718ea4ca65b586817c
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-10-08 20:57:10 +0200

    Added annotations, getters and setters in Game entity.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Game.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/Game.java
@@ -1,8 +1,42 @@
 package com.github.robertsawyer.CulturalChronicles.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "games")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Game {
 
     private Long id;
     private String title;
     private String genre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
