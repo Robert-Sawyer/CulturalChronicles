16d4b99f55025cda5637523cf80ded8ef4e832e4
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-10-08 20:57:35 +0200

    Added extension in GameRepository.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/domain/repositories/GameRepository.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/repositories/GameRepository.java
@@ -1,4 +1,9 @@
 package com.github.robertsawyer.CulturalChronicles.domain.repositories;
 
-public interface GameRepository {
+import com.github.robertsawyer.CulturalChronicles.domain.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+
 }
