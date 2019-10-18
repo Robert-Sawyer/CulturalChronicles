48f9f128139b8b6b0155ebcaa9be99d7a25362fc
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-10-18 22:13:05 +0200

    Created new entity - CastMember with fields and annotations.

---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/CastMember.java
@@ -0,0 +1,96 @@
+package com.github.robertsawyer.CulturalChronicles.domain.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "actors")
+@NoArgsConstructor
+@AllArgsConstructor
+public class CastMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Character sex;
+
+    @Column
+    private Date birthDate;
+
+    @Column
+    private Date dearhDate;
+
+    @Column
+    @ElementCollection(targetClass = Movie.class)
+    private Set<Movie> movies;
+
+    @Column
+    private Boolean favourite;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Character getSex() {
+        return sex;
+    }
+
+    public void setSex(Character sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Date getDearhDate() {
+        return dearhDate;
+    }
+
+    public void setDearhDate(Date dearhDate) {
+        this.dearhDate = dearhDate;
+    }
+
+    public Set<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(Set<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Boolean getFavourite() {
+        return favourite;
+    }
+
+    public void setFavourite(Boolean favourite) {
+        this.favourite = favourite;
+    }
+}
