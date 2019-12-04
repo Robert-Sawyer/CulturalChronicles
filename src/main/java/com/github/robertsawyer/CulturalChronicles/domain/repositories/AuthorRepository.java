ec6a78798921394a04a431ad240da48e39b38043
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-12-04 20:41:03 +0100

    Corrected name od AuthorRepository, changed type of argument from Author to String.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/domain/repositories/AuthorRepository.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/repositories/AuthorRepository.java
@@ -4,13 +4,13 @@ import com.github.robertsawyer.CulturalChronicles.domain.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface AuthorReepository extends JpaRepository<Author, Long> {
+public interface AuthorRepository extends JpaRepository<Author, Long> {
 
 
     Author checkAuthor(String author);
 
     @Query(value = "SELECT * FROM authors WHERE name = ?",
     nativeQuery = true)
-    Author findByAuthorName(Author author);
+    Author findByAuthorName(String name);
 
 }
--- src/main/java/com/github/robertsawyer/CulturalChronicles/services/AuthorService.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/services/AuthorService.java
@@ -1,7 +1,7 @@
 package com.github.robertsawyer.CulturalChronicles.services;
 
 import com.github.robertsawyer.CulturalChronicles.domain.model.Author;
-import com.github.robertsawyer.CulturalChronicles.domain.repositories.AuthorReepository;
+import com.github.robertsawyer.CulturalChronicles.domain.repositories.AuthorRepository;
 import com.github.robertsawyer.CulturalChronicles.dto.AddAuthorDTO;
 import com.github.robertsawyer.CulturalChronicles.dto.FindAuthorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +15,7 @@ import java.util.Set;
 public class AuthorService {
 
     @Autowired
-    private AuthorReepository authorReepository;
+    private AuthorRepository authorRepository;
 
     public boolean checkAuthor(Set<Author> authors) {
         if (authors.isEmpty()) {
@@ -24,7 +24,7 @@ public class AuthorService {
         try {
             for (Author newAuthor : authors) {
                 String author = newAuthor.toString();
-                Author checkedAuthor = authorReepository.checkAuthor(author);
+                Author checkedAuthor = authorRepository.checkAuthor(author);
                 if (checkedAuthor == null) {
                     return false;
                 }
@@ -38,17 +38,17 @@ public class AuthorService {
     @Transactional
     public void createNewAuthor(AddAuthorDTO addAuthorDTO) {
         Author author = Converters.convertToNewAuthor(addAuthorDTO);
-        authorReepository.save(author);
+        authorRepository.save(author);
     }
 
     public List<Author> getAllAuthors() {
-        List<Author> authors = authorReepository.findAll();
+        List<Author> authors = authorRepository.findAll();
         return authors;
     }
 
     public Author findByName(FindAuthorDTO authorName) {
-        Author author = Converters.convertFindAuthorDTOtoAuthor(authorName);
-        return authorReepository.findByAuthorName(author);
+//        Author author = Converters.convertFindAuthorDTOtoAuthor(authorName);
+        return authorRepository.findByAuthorName(authorName.getName());
     }
 
 
