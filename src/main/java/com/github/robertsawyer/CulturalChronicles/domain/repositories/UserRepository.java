964f6c9e90f2b1eb9fd77c0b60b3a238d53c887f
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-12-05 21:25:25 +0100

    Added method in UserRepository.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/domain/repositories/UserRepository.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/repositories/UserRepository.java
@@ -1,4 +1,9 @@
 package com.github.robertsawyer.CulturalChronicles.domain.repositories;
 
-public interface UserRepository {
+import com.github.robertsawyer.CulturalChronicles.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByName(String name);
 }
