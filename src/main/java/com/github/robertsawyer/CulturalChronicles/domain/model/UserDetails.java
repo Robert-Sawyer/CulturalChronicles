0f64c0c7bacf57b4ae0b2827724532a95c5957af
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-12-29 20:39:36 +0100

    Added fields, contstructors, getters and setters.

--- src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/UserDetails.java
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/domain/model/UserDetails.java
@@ -1,9 +1,72 @@
 package com.github.robertsawyer.CulturalChronicles.domain.model;
 
+import java.util.Date;
+
 public class UserDetails {
 
     private Long id;
     private String firstName;
     private String lastName;
+    private Gender gender;
+    private Date birthDate;
+    private String city;
 
+    public UserDetails() {
+    }
+
+    public UserDetails(String firstName, String lastName, Gender gender, Date birthDate, String city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
