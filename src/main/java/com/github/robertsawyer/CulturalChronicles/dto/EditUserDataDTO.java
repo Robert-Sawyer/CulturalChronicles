f0daa38bb6e6fd5e0d7a90cf0c4560206d9d5d06
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2019-12-30 15:53:36 +0100

    Created editUserDataDTO.

---
+++ src/main/java/com/github/robertsawyer/CulturalChronicles/dto/EditUserDataDTO.java
@@ -0,0 +1,72 @@
+package com.github.robertsawyer.CulturalChronicles.dto;
+
+import com.github.robertsawyer.CulturalChronicles.domain.model.Gender;
+
+import java.util.Date;
+
+public class EditUserDataDTO {
+
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private Gender gender;
+    private String email;
+    private Date birthDate;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
